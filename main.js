a79c11351df0acce368dbcd385acc35b600a173b
Asilbek777 <raimovasilbek707@gmail.com>
2022-10-21 19:35:05 +0500

    Darajani hisoblash

---
+++ main.js
@@ -0,0 +1,3 @@
+a = prompt("Assalomu alaykum.Men sizning botingiz bo`laman. Bu bot sonning darajasini topib beradi.Menga birinch bo`lib Sonni kirgizing")
+b = prompt("Endi esa sonning darajasini kirgizing")
+document.write(a**b)

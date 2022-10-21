daf6c845a6a5618ce85f4b26084dc9a4a4788a3d
Asilbek777 <raimovasilbek707@gmail.com>
2022-10-21 20:01:55 +0500

    add .

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-a = prompt("Assalomu alaykum.Men sizning botingiz bo`laman. Bu bot sonning darajasini topib beradi.Menga birinch bo`lib Sonni kirgizing")
+a = prompt("Assalomu alaykum.Men sizning botingiz bo`laman. Bu bot sonning darajasini topib beradi.Menga birinchi bo`lib Sonni kirgizing")
 b = prompt("Endi esa sonning darajasini kirgizing")
 document.write(a**b)
